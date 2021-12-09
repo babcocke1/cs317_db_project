ab1b32b46a01c8d8cc82ee5c47e4d06f88e2844f
eric babcock <babcoce1@my.erau.edu>
2021-12-09 12:23:40 -0700

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get install -y apache2
 
-echo \n1\n1\n1
+
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
@@ -12,6 +12,7 @@ apt-get install -y python3
 apt-get install -y python3-pip
 pip3 install --upgrade setuptools
 apt-get install -y mariadb-server
+python3 -m pip install --upgrade pip
 pip3 install mysqlclient
 apt-get install -y -mod-php
 apt-get install -y -mod-php
